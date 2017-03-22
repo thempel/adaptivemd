296500ed7dcdb9daa9a72e964b2c0901e15bde8b
thempel <thempel@zedat.fu-berlin.de>
2017-03-22 14:25:20 +0100

    [analysis.pyemma._remote] working analysis script

--- adaptivemd/analysis/pyemma/_remote.py
+++ adaptivemd/analysis/pyemma/_remote.py
@@ -1,6 +1,8 @@
+from adaptivemd import Model
+
+
 def remote_analysis(
-        trajectories,
-        traj_name='output.dcd',
+        files,
         topfile='input.pdb',
         tica_lag=2,
         tica_dim=2,
@@ -12,10 +14,8 @@ def remote_analysis(
 
     Parameters
     ----------
-    trajectories : list of `Trajectory`
+    files : list of `Trajectory`
         a list of `Trajectory` objects
-    traj_name : str
-        name of the trajectory file with the trajectory directory given
     topfile : `File`
         a reference to the `.pdb` file using in pyemma
     tica_lag : int
@@ -35,28 +35,82 @@ def remote_analysis(
         a model object with a data attribute which is a dict and contains all relevant
         information about the computed MSM
     """
-    import os
-
     import pyemma
-    from adaptivemd import Model
+    import numpy as np
 
     feat = pyemma.coordinates.featurizer(topfile)
-    feat.add_backbone_torsions()
+
+    selection = feat.select('protein and mass > 2')[29:-30] 
+    feat.add_selection(selection)
 
     pyemma.config.show_progress_bars = False
 
-    print '#trajectories :', len(trajectories)
+    # todo: allow specification of several folders and wildcats, used for session handling
+    # if isinstance(trajfiles, basestring):
+    #     if '*' in trajfiles or trajfiles.endswith('/'):
+    #         files = glob.glob(trajfiles)
 
-    files = [os.path.join(t, traj_name) for t in trajectories]
+    print '#files :', len(files)
+
     inp = pyemma.coordinates.source(files, feat)
 
-    tica_obj = pyemma.coordinates.tica(
-        inp, lag=tica_lag, dim=tica_dim, kinetic_map=False)
+    #tica_obj = pyemma.coordinates.tica(
+    #    inp, lag=tica_lag, dim=tica_dim, kinetic_map=False)
 
-    y = tica_obj.get_output()
+    y = inp.get_output()
 
-    cl = pyemma.coordinates.cluster_kmeans(data=y, k=msm_states, stride=stride)
+    ### BEGIN GUILLE'S CODE
+    def regspace_cluster_to_target(data, n_clusters_target,
+                                   n_try_max=15, delta=5.,
+                                   verbose=True, stride=1):
+        r"""
+        Clusters a dataset to a target n_clusters using regspace clustering by iteratively. "
+        Work best with 1D data
+        data: ndarray or list thereof
+        n_clusters_target: int, number of clusters.
+        n_try_max: int, default is 5. Maximum number of iterations in the heuristic.
+        delta: float, defalut is 5. Percentage of n_clusters_target to consider converged.
+                 Eg. n_clusters_target=100 and delta = 5 will consider any clustering between 95 and 100 clustercenters as
+                 valid. Note. Note: An off-by-one in n_target_clusters is sometimes unavoidable
+        returns: pyemma clustering object
+        tested:True
+        """
+        delta = delta/100
+
+        assert np.vstack(data).shape[0] >= n_clusters_target, "Cannot cluster " \
+                                                          "%u datapoints on %u clustercenters. Reduce the number of target " \
+                                                          "clustercenters."%(np.vstack(data).shape[0], n_clusters_target)
+        # Works well for connected, 1D-clustering,
+        # otherwise it's bad starting guess for dmin
+        # cmax = np.vstack(data).max()
+        # cmin = np.vstack(data).min()
+        # dmin = (cmax-cmin)/(n_clusters_target+1)
+        dmin = .25
+        err = np.ceil(n_clusters_target*delta)
+        cl = pyemma.coordinates.cluster_regspace(data, dmin=dmin, stride=stride, metric='minRMSD')
+        for cc in range(n_try_max):
+            n_cl_now = cl.n_clusters
+            delta_cl_now = np.abs(n_cl_now - n_clusters_target)
+            if not n_clusters_target-err <= cl.n_clusters <= n_clusters_target+err:
+                # Cheap (and sometimes bad) heuristic to get relatively close relatively quick
+                dmin = cl.dmin*cl.n_clusters/   n_clusters_target
+                cl = pyemma.coordinates.cluster_regspace(data, dmin=dmin, metric='minRMSD', max_centers=5000, stride=stride)# max_centers is given so that we never reach it (dangerous)
+            else:
+                break
+            if verbose:
+                print('cl iter %u %u -> %u (Delta to target (%u +- %u): %u'%(cc, n_cl_now, cl.n_clusters,
+                                                                             n_clusters_target, err, delta_cl_now))
+        return cl
+    ### END GUILLE'S CODE
+
+    #cl = regspace_cluster_to_target(y, msm_states,
+    #                                delta=int(msm_states/10),
+    #                                stride=stride)
+    cl = pyemma.coordinates.cluster_regspace(data=y, dmin=.22, stride=stride, max_centers=500, metric='minRMSD')
     m = pyemma.msm.estimate_markov_model(cl.dtrajs, msm_lag)
+
+    #cl = pyemma.coordinates.cluster_kmeans(data=y, k=msm_states, stride=stride)
+
 
     data = {
         'input': {
@@ -65,12 +119,9 @@ def remote_analysis(
             'n_trajectories': inp.number_of_trajectories(),
             'lengths': inp.trajectory_lengths(),
         },
-        'tica': {
-            'dimension': tica_obj.dimension(),
-            'lagtime': tica_lag
-        },
+
         'clustering': {
-            'k': msm_states,
+            'k': cl.n_clusters,
             'dtrajs': [
                 t for t in cl.dtrajs
             ]
